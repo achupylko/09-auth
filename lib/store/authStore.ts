76b6e800ee39ddd76d53ef430485d2f1b91a1c3f
Artem Chupylko <achupylko@gmail.com>
2026-05-14 19:31:04 +0000

    feat(auth-store): add authentication state management

---
+++ lib/store/authStore.ts
@@ -0,0 +1,32 @@
+import { User } from '@/types/user';
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+type AuthStore = {
+  user: User | null;
+  isAuthenticated: boolean;
+  setUser: (user: User) => void;
+  clearIsAuthenticated: () => void;
+};
+
+export const useAuthStore = create<AuthStore>()(
+  persist(
+    set => ({
+      user: null,
+      isAuthenticated: false,
+
+      setUser: user =>
+        set({
+          user,
+          isAuthenticated: true,
+        }),
+
+      clearIsAuthenticated: () =>
+        set({
+          user: null,
+          isAuthenticated: false,
+        }),
+    }),
+    { name: 'auth-store' }
+  )
+);
