1539e781ca065bb806a5630cd675b34e5c990257
Artem Chupylko <achupylko@gmail.com>
2026-05-17 14:57:53 +0000

    feat(auth): add protected routes and session validation

--- lib/api/serverApi.ts
+++ lib/api/serverApi.ts
@@ -1,5 +1,7 @@
 import { Note } from '@/types/note';
 import { nextServer } from './api';
+import { cookies } from 'next/headers';
+import { User } from '@/types/user';
 
 const TOKEN = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
 
@@ -44,7 +46,28 @@ export const fetchNoteById = async (id: string) => {
 };
 
 // get me
-export const getMe = async () => {};
+export const getServerMe = async (): Promise<User> => {
+  const cookieStore = await cookies();
+  const { data } = await nextServer.get('/users/me', {
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+  });
+  return data;
+};
 
-// check session
-export const checkSession = async () => {};
+// check server session
+export const checkServerSession = async () => {
+  // get current cookies
+  const cookieStore = await cookies();
+
+  const response = await nextServer.get('/auth/session', {
+    headers: {
+      // pass the cookie on
+      Cookie: cookieStore.toString(),
+    },
+  });
+
+  // return the full response so that the proxy has access to the new cookies
+  return response;
+};
