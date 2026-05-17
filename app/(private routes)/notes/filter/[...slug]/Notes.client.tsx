ab38b87c9977bc9286269947fdf677f19702da74
Artem Chupylko <achupylko@gmail.com>
2026-05-17 17:58:37 +0300

    feat(auth): add protected routes and session validation (#8)

--- app/(private routes)/notes/filter/[...slug]/Notes.client.tsx
+++ app/(private routes)/notes/filter/[...slug]/Notes.client.tsx
@@ -6,7 +6,7 @@ import Loader from '@/components/Loader/Loader';
 import NoteList from '@/components/NoteList/NoteList';
 import Pagination from '@/components/Pagination/Pagination';
 import SearchBox from '@/components/SearchBox/SearchBox';
-import { fetchNotes } from '@/lib/api';
+import { fetchNotes } from '@/lib/api/clientApi';
 import { keepPreviousData, useQuery } from '@tanstack/react-query';
 import React, { useState } from 'react';
 import { useDebouncedCallback } from 'use-debounce';
