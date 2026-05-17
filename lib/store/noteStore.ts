5630a4cd5a90d41af72ee2332a8a341a1aee87e8
Artem Chupylko <achupylko@gmail.com>
2026-05-17 20:14:00 +0300

    Fix/note store (#11)
    * fix(nextjs): fix production build issue
    
    * fix(note-store): fix NewNote type import

--- lib/store/noteStore.ts
+++ lib/store/noteStore.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
-import { NewNote } from '../api';
+import { NewNote } from '../api/clientApi';
 
 type NoteDraftStore = {
   draft: NewNote;
